dccf69517fe1366c02e12c941088e5156839a75b
Jaydeep <jaydeepshome06@gmai.com>
2021-04-29 21:16:07 +0530

    Remote state file added

--- staging/main.tf
+++ staging/main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-west-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "staging-testnew"
+    key = "staging.tfstate"
+    region = "us-west-1"
+  }
+}
+
 module "vpc" {
   source = "../modules/vpc"
   infra_env = "staging"
