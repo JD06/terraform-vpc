85f5356b996efccd24c4c8a9bbd97ec82463f744
Jaydeep <jaydeepshome06@gmai.com>
2021-07-31 13:49:17 +0530

    Changes in the version of the module

--- staging/main.tf
+++ staging/main.tf
@@ -1,15 +1,14 @@
-terraform {
+/*terraform {
   backend "s3" {
     bucket = "staging-testnew"
-    key = "staging.tfstate"
+    key    = "staging.tfstate"
     region = "us-west-1"
   }
-}
+}*/
 
 module "vpc" {
-  source = "github.com/JD06/terraform-module-vpc.git?ref=v1.1.0"
+  source    = "github.com/JD06/terraform-module-vpc.git?ref=v3.0.0"
   infra_env = "staging"
-  name = "mystaging"
-  vpc_cidr = "10.0.0.0/16"
+  name      = "mystaging"
+  vpc_cidr  = "10.0.0.0/16"
 }
-
